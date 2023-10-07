37c7b3cf621de5abc22ddec000986485a3001395
Kristine Hill <kvessey13@gmail.com>
2023-10-07 00:25:05 +0000

    Completed prep-javascript-operators-and-expressions exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 3;
+const number2 = 7;
+const product = number1 * number2;
+console.log('Value of product:', product);
+console.log('Type of product:', typeof product);
+
+const charge = 13;
+const payment = 11;
+const amountRemaining = charge - payment;
+console.log('Value of amountRemaining:', amountRemaining);
+console.log('Type of amountRemaining:', typeof amountRemaining);
+
+const tests = 85;
+const assignments = 100;
+const final = 95;
+const grade = (tests + assignments + final) / 3;
+console.log('Value of grade:', grade);
+console.log('Type of grade:', typeof grade);
+
+const firstName = 'Kristine';
+const lastName = 'Hill';
+const fullName = firstName + ' ' + lastName;
+console.log('Value of full name:', fullName);
+console.log('Type of full name:', typeof fullName);
+
+const ph = 3;
+const isAcidic = ph < 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('Type of isAcidic:', typeof isAcidic);
+
+const numberOfSoldiers = 113;
+const isSparta = numberOfSoldiers === 300;
+console.log('Value of isSparta:', isSparta);
+console.log('Type of isSparta:', typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log('Value of nickname:', nickname);
+console.log('Type of nickname:', typeof nickname);
